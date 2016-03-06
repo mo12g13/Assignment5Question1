fa05355bc5b5f9d4d34b5323a18fea3f46365685
Momo Johnson <mo12g13@gmail.com>
2016-03-06 12:33:33 -0600

    Final, I only need to caught negative validation

--- src/com/Momo/Main.java
+++ src/com/Momo/Main.java
@@ -5,7 +5,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import java.util.InputMismatchException;
+
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,6 +17,13 @@ public class Main {
         String teaName;
         Double priceOfTea;
         int numberItem=0;
+        double totalProfit =0.0;
+        double revenueTotal =0.0;
+        double expensesTotal = 0.0;
+        double total1 =0.0;
+        double total2 =0.0;
+
+
         String[] lineSeperate;
         Double cost;
 
@@ -34,35 +44,44 @@ public class Main {
                 String line3 = lineSeperate[2];
                 priceOfTea = Double.parseDouble(line3);
 
-
                 do {
                     try {
                         System.out.println("How many " + teaName + " did you today?");
 
                         numberItem = input.nextInt();
-                        if(numberItem>0) {
+                        
 
                             istrue = false;
-                        }
+
                     } catch (InputMismatchException e) {
                         input.nextLine();
                         System.out.println("Please enter right number");
                         istrue = true;
                     }
+                    catch (NoSuchElementException e){
+                        System.out.println(" Please enter positive integers");
+                    }
                 }while (istrue);
-                mySalesReport.write(String.format("%s sold: %d, Expenses: $%.2f, Revenue: $%.2f%n", teaName,
-                        numberItem, expenses(cost, numberItem),  totalRevnue(priceOfTea, numberItem )));
-
-
-
+                mySalesReport.write(String.format("%s sold: %d, Expenses: $%.2f, Revenue: $%.2f, Profit: $%.2f%n", teaName,
+                        numberItem, expenses(cost, numberItem),  totalRevnue(priceOfTea, numberItem ), totalRevnue(priceOfTea, numberItem)-expenses(cost,numberItem) ));
+                total1 = totalRevnue(priceOfTea, numberItem);
+                revenueTotal = revenueTotal+total1;
+                total2  = expenses(cost, numberItem);
+                expensesTotal = expensesTotal+total2;
+               totalProfit = revenueTotal-expensesTotal;
+                line = report.readLine();
             }
+            mySalesReport.write(String.format("Total Revenue: $%.2f%n", revenueTotal ));
+            mySalesReport.write(String.format("Total Expenses: $%.2f%n", expensesTotal));
+            mySalesReport.write(String.format("Total Profit:  $%.2f%n",  totalProfit));
+
             mySalesReport.close();
             report.close();
         } catch (IOException e) {
             System.out.println("File not found");
         }
     }
-    //A method that calculates and returns the expenses for the eact product
+    //A method that calculates and returns the expenses
     public static double expenses(double cost, int typeSold ){
         double totalExpeses = cost*typeSold;
         return totalExpeses;
